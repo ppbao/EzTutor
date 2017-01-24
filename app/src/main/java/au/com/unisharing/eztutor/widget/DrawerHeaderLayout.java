45c3ad9124bad1a6806c7bec42bdb357b738c752
ppbao <bruce.liu.02@gmail.com>
2017-01-24 17:04:55 +1000

    edit drawerheader

--- app/src/main/java/au/com/unisharing/eztutor/widget/DrawerHeaderLayout.java
+++ app/src/main/java/au/com/unisharing/eztutor/widget/DrawerHeaderLayout.java
@@ -1,6 +1,7 @@
 package au.com.unisharing.eztutor.widget;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 
 import com.mikepenz.materialdrawer.Drawer;
 
+import au.com.unisharing.eztutor.FragmentIntent;
 import au.com.unisharing.eztutor.R;
 import au.com.unisharing.eztutor.model.User;
 import au.com.unisharing.eztutor.utils.ViewUtils;
@@ -21,7 +23,7 @@ import au.com.unisharing.eztutor.utils.ViewUtils;
  * Description  :
  */
 
-public class DrawerHeaderLayout extends LinearLayout {
+public class DrawerHeaderLayout extends LinearLayout implements View.OnClickListener{
 
     private UserPhotoView userPhotoView;
     private TextView tvNickname;
@@ -31,6 +33,7 @@ public class DrawerHeaderLayout extends LinearLayout {
     private ImageView ivNotification;
     private View vRecordView;
     private View vRecordAudio;
+    private View vRecordVideo;
     private View vUploadLocal;
     private View vLoginWrapper;
     private View vUserWrapper;
@@ -92,5 +95,52 @@ public class DrawerHeaderLayout extends LinearLayout {
         super.onFinishInflate();
         userPhotoView = (UserPhotoView) findViewById(R.id.user_photo_view);
         tvNickname = (TextView) findViewById(R.id.tv_nickname);
+        tvUserName = (TextView) findViewById(R.id.tv_username);
+        ivEditProfile = (ImageView) findViewById(R.id.iv_edit_profile);
+        ivShowResumables = (ImageView) findViewById(R.id.iv_show_resumables);
+        ivNotification = (ImageView) findViewById(R.id.iv_notification);
+        vLoginWrapper = findViewById(R.id.v_login_wrapper);
+        vRecordAudio = findViewById(R.id.v_record_audio);
+        vRecordVideo = findViewById(R.id.v_record_video);
+        vUploadLocal = findViewById(R.id.v_upload_local);
+
+        vRecordAudio.setOnClickListener(this);
+        vRecordVideo.setOnClickListener(this);
+        vUploadLocal.setOnClickListener(this);
+        ivEditProfile.setOnClickListener(this);
+        ivNotification.setOnClickListener(this);
+        ivShowResumables.setOnClickListener(this);
+        vLoginWrapper.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (drawer != null) {
+            drawer.closeDrawer();
+        }
+        FragmentIntent intent;
+        Bundle bundle;
+        switch (v.getId()){
+            case R.id.user_photo_view:
+                break;
+            case R.id.iv_edit_profile:
+                break;
+            case R.id.v_login_wrapper:
+                break;
+            case R.id.v_record_video:
+                break;
+            case R.id.v_record_audio:
+                break;
+            case R.id.v_upload_local:
+                break;
+            case R.id.iv_notification:
+                break;
+            case R.id.iv_show_resumables:
+                break;
+            default:
+                return;
+        }
+
     }
 }
